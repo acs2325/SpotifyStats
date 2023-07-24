0d5c2635cd107c4899206770c730a6318ee88281
Andrew Caldon Smith <andrew.caldon.smith@cern.ch>
2023-07-24 11:17:52 -0400

    default values for bash script

--- artist_by_listens.sh
+++ artist_by_listens.sh
@@ -1,1 +1,5 @@
-cat MyData/StreamingHist*.json | grep "artist" | sort | uniq -c | awk -F"\"" '{ print $1 $4}' | sort -n -r  > artist_by_listens.txt
+VAR=${1:-andrew} 
+VAR2=${2:-10} 
+cat ./$VAR/MyData/StreamingHist*.json | grep "artist" | sort | uniq -c | awk -F"\"" '{ print $1 $4}' | sort -n -r  > artist_by_listens.txt
+
+head -$VAR2 artist_by_listens.txt 
