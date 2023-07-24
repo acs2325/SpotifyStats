97e0f9f1f94b77439396c826f8dbc6a514d29344
Andrew Caldon Smith <andrew.caldon.smith@cern.ch>
2023-07-24 11:19:55 -0400

    print formatting in bash script

--- artist_by_listens.sh
+++ artist_by_listens.sh
@@ -1,5 +1,9 @@
 VAR=${1:-andrew} 
 VAR2=${2:-10} 
+
+echo "========================================"
+echo "user: $VAR's top $VAR2 artists by N listens"
+echo "========================================"
 cat ./$VAR/MyData/StreamingHist*.json | grep "artist" | sort | uniq -c | awk -F"\"" '{ print $1 $4}' | sort -n -r  > artist_by_listens.txt
 
 head -$VAR2 artist_by_listens.txt 
